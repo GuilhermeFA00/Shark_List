fe7105ad99aef2791cbe90a5a13a1f380704e6e6
GuilhermeFA00 <sharkdevv@gmail.com>
2021-07-13 16:04:34 -0300

    update!

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -11,9 +11,43 @@ btn.onkeyup = () => {
         btn.classList.remove("active");
     }
 }
+showTasks();
+
+
+btn.onclick = () => {
+    let userInput = input.value;
+    let localStrg = localStorage.getItem("New note!");
+    if (localStrg == null) {
+        listArray = [];
+    } else {
+        listArray = JSON.parse(localStrg);
+    }
+    listArray.push(userInput);
+    localStrg.setItem("New note", JSON.stringify(listArray));
+    showTasks();
+    btn.classList.remove("active");
+}
 
 
 function showTasks() {
     let localStrg = localStorage.getItem("New note!");
+    if (localStrg == null) {
+        listArray = [];
+    } else {
+        listArray = JSON.parse(localStrg)
+    }
 
+    const pendingTasksNumb = document.querySelector(".your-tasks");
+    pendingTasksNumb.textContent = listArray.length;
+    if (listArray.length > 0) {
+        del.classList.add("active");
+    } else {
+        del.classList.remove("active");
+    }
+    let newLi = "";
+    listArray.forEach((el, index) => {
+        newLi += `<li>${element}<span class="icon" onclick="deleteTask(${index})"><i class="fas fa-trash"></i></span></li>`
+    });
+    list.innerHTML = newLi;
+    input.value = "";
 }
