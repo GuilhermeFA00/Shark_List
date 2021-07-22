ad859fdc7ab902dbd7d058ff26422d0ff9182da2
GuilhermeFA00 <sharkdevv@gmail.com>
2021-07-22 20:59:15 -0300

    update!

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -233,26 +233,26 @@ class NoteManager {
 const noteManager = new NoteManager({
     el: document.getElementById('notesWrapper'),
     notes: [
-        {
-            title: 'sunt aut facere repellat',
-            body: 'uia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto'
-        },
-        {
-            title: 'qui est esse',
-            body: 'est rerum tempore vitae<br>nsequi sint nihil reprehenderit dolor beatae ea dolores neque <br>fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis<br>qui aperiam non debitis possimus qui neque nisi nulla'
-        },
-        {
-            title: 'nesciunt quas odio',
-            body: 'repudiandae veniam quaerat sunt sed alias aut fugiat sit autem sed est'
-        },
-        {
-            title: 'This is a demo note',
-            body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi corrupti officiis alias tenetur, tenetur iste maxime laudantium?'
-        },
-        {
-            title: 'qui est esse',
-            body: 'est rerum tempore vitae<br>nsequi sint nihil reprehenderit dolor beatae ea dolores neque <br>fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis<br>qui aperiam non debitis possimus qui neque nisi nulla'
-        },
+        //     {
+        //         title: 'sunt aut facere repellat',
+        //         body: 'uia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto'
+        //     },
+        //     {
+        //         title: 'qui est esse',
+        //         body: 'est rerum tempore vitae<br>nsequi sint nihil reprehenderit dolor beatae ea dolores neque <br>fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis<br>qui aperiam non debitis possimus qui neque nisi nulla'
+        //     },
+        //     {
+        //         title: 'nesciunt quas odio',
+        //         body: 'repudiandae veniam quaerat sunt sed alias aut fugiat sit autem sed est'
+        //     },
+        //     {
+        //         title: 'This is a demo note',
+        //         body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi corrupti officiis alias tenetur, tenetur iste maxime laudantium?'
+        //     },
+        //     {
+        //         title: 'qui est esse',
+        //         body: 'est rerum tempore vitae<br>nsequi sint nihil reprehenderit dolor beatae ea dolores neque <br>fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis<br>qui aperiam non debitis possimus qui neque nisi nulla'
+        //     },
     ]
 });
 
